736e76ab57641c4fb77eac2f25cbccbf16fdd595
Pamela Olney <pamelaolney@s05692-mbpr.attlocal.net>
2020-06-29 16:32:21 -0400

    Added contact card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import "./styles.css";
 
 const App = () => {
   const message = "This is our React app - it's great";
 
+  const handelClick = () => {
+    alert("you clicked a message");
+  }
+
   return(
-    <div>
-      <h1> Hello React World</h1>
-      <h2 onClick={()=> alert("you clicked the message")}>
-        {message}
-      </h2>
+    <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className="user-details">
+        <p>Name: Jenny Han</p>
+        <p>Email: Jenny.Han@notreal.com</p>
+        <p>Age: 25</p>
+      </div>
     </div>
   );
 }
